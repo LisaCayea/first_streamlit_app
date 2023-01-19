fa1dba11bfbacbbb4704f247384067e7ede8663b
LisaCayea <119895323+LisaCayea@users.noreply.github.com>
2023-01-19 10:13:29 -0500

    updated text
    to include other menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Ogmea 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Eggs')
